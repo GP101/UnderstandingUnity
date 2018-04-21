3a26044b3d44d01f588a03e3065c94af58a371a1
jintaeks <jintaeks@gmail.com>
2018-04-21 22:11:49 +0900

    first commit
    Understanding Unity Game Engine by 13FGames

---
+++ UnderstandingUnityEngine/Reference Files/Program GameObject01 inherits interfaces.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingUnityEngine
+{
+    public class Component
+    {
+    }
+
+    public abstract class MonoBehavior : Component
+    {
+        public List<Component> m_components;
+        public MonoBehavior()
+        {
+            m_components = new List<Component>();
+        }
+        public T GetComponent<T>() where T : Component
+        {
+            foreach (var comp in m_components)
+            {
+                if (comp.GetType() == typeof(T))
+                {
+                    return (T)comp;
+                }
+            }
+            return default(T);
+        }
+    }
+
+    public interface BoxCollider
+    {
+        void GetCollider();
+    }
+    public interface MeshRenderer
+    {
+        void GetMaterial();
+    }
+
+    public class GameObject : BoxCollider, MeshRenderer
+    {
+        public void GetCollider() { }
+        public void GetMaterial() { }
+
+        // Use this for initialization
+        public void Start()
+        {
+        }
+
+        public void Update()
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameObject e = new GameObject();
+            e.Start();
+            e.Update();
+        }
+    }
+}
---
+++ UnderstandingUnityEngine/Reference Files/Program GameObject02 GetComponent.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingUnityEngine
+{
+    public class Component
+    {
+    }
+
+    public abstract class MonoBehavior
+    {
+        public List<Component> m_components;
+        public MonoBehavior()
+        {
+            m_components = new List<Component>();
+        }
+        public T GetComponent<T>() where T : Component
+        {
+            foreach (var comp in m_components)
+            {
+                if (comp.GetType() == typeof(T))
+                {
+                    return (T)comp;
+                }
+            }
+            return default(T);
+        }
+    }
+
+    public class BoxCollider : Component
+    {
+        public void GetCollider() { }
+    }
+    public class MeshRenderer : Component
+    {
+        public void GetMaterial()
+        {
+            Console.WriteLine("GetMaterial");
+        }
+    }
+
+    public class GameObject : MonoBehavior
+    {
+        public GameObject()
+        {
+            m_components.Add(new BoxCollider());
+            m_components.Add(new MeshRenderer());
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Update()
+        {
+            MeshRenderer renderer = GetComponent<MeshRenderer>();
+            renderer.GetMaterial();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameObject e = new GameObject();
+            e.Start();
+            e.Update();
+        }
+    }
+}
