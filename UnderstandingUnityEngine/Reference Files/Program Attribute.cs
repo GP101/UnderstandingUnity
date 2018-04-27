1e0d7e1945d3c32f8d2b2c92a294037d1437d37e
jintaeks <jintaeks@gmail.com>
2018-04-27 21:42:59 +0900

    Attribute, LateUpdate, Update, FixedUpdate
    _20180427_jintaeks

---
+++ UnderstandingUnityEngine/Reference Files/Program Attribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Input;
+using System.Threading;
+
+namespace UnderstandingUnityEngine
+{
+    class Program
+    {
+        class Test
+        {
+            [Obsolete("Use Foo2()", true)]
+            public void Foo()
+            {
+            }
+            [Sample(Name = "Hello")]
+            public void Foo2()
+            {
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            t.Foo2();
+        }
+
+        //[AttributeUsage(AttributeTargets.Class)]
+        public class SampleAttribute : Attribute
+        {
+            public string Name { get; set; }
+        }
+    }
+}
