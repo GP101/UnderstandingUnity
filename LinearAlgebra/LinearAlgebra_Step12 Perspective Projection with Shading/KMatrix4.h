9dd315d94e78fb672b45e4375bf6f2ad58aa23b0
jintaeks <jintaeks@gmail.com>
2018-07-05 16:44:55 +0900

    Added Step12 Shading. _20180705_jintaeks

---
+++ LinearAlgebra/LinearAlgebra_Step12 Perspective Projection with Shading/KMatrix4.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "KVector3.h"
+
+class KMatrix4
+{
+public:
+    float   m_afElements[4][4];
+
+	KMatrix4();
+	~KMatrix4();
+
+    /// access matrix element (iRow,iCol)
+    /// @param  iRow: row index
+    /// @param  iCol: column index
+    /// @return reference of element (iRow,iCol)
+    float& operator()(int iRow, int iCol);
+    KMatrix4 operator*(KMatrix4& mRight);
+    KVector3 operator*(KVector3& vRight);
+    KMatrix4 operator+(KMatrix4& mRight);
+    KMatrix4& operator=(KMatrix4& mRight);
+
+    KMatrix4 SetZero();
+    KMatrix4 SetIdentity();
+    KMatrix4 SetRotationX(float fRadian);
+    KMatrix4 SetRotationY(float fRadian);
+    KMatrix4 SetRotationZ(float fRadian);
+    KMatrix4 SetScale(float fxScale, float fyScale, float fzScale);
+    KMatrix4 SetTranslation(float x, float y, float z);
+
+    KMatrix4 SetProjection(float d);
+    KMatrix4 SetProjection( const float near_plane // Distance to near clipping plane
+        , const float far_plane  // Distance to far clipping plane
+        , const float fov_horiz  // Horizontal field of view angle, in radians
+        , const float fov_vert );   // Vertical field of view angle, in radians
+};//class KMatrix4
