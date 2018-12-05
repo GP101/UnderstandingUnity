ae568902cbe5f7da00cd07696198d0669fa70466
jintaeks <jintaeks@gmail.com>
2018-12-05 09:15:02 +0900

    Data structure tree

---
+++ Data Structure Tree/Traverse.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traverse : MonoBehaviour {
+    private Queue<Transform>   m_queue;
+
+    void TreeTraverse( Transform go )
+    {
+        foreach( Transform child in go )
+        {
+            //Debug.Log( child.name ); // pre-order
+            TreeTraverse( child );
+            Debug.Log( child.name ); // post-order
+        }
+    }
+
+    void LevelOrder( Transform go )
+    {
+        m_queue.Clear();
+        m_queue.Enqueue( go );
+        while( m_queue.Count >= 1 )
+        {
+            Transform node = m_queue.Dequeue();
+            Debug.Log( node.name );
+            foreach( Transform child in node)
+            {
+                m_queue.Enqueue( child);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        //TreeTraverse( transform );
+        m_queue = new Queue<Transform>();
+        LevelOrder( transform );
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
