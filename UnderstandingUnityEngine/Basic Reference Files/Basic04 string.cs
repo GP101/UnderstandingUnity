6cdf9b901e871a508b49f84b4ddc63ea052e105b
jintaeks <jintaeks@gmail.com>
2019-03-11 08:53:38 +0900

    Added basic C# reference files.

---
+++ UnderstandingUnityEngine/Basic Reference Files/Basic04 string.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main( string[] args )
+    {
+        //from string literal and string concatenation
+        string fname, lname;
+        fname = "John";
+        lname = "Seo";
+
+        char[] letters= { 'H', 'e', 'l', 'l','o' };
+        string[] sarray={ "Hello", "From", "Tutorials", "Point" };
+        //string[] sarray=new string[4]{ "Hello", "From", "Tutorials", "Point" };
+
+        string fullname = fname + lname;
+        Console.WriteLine( "Full Name: {0}", fullname );
+
+        //by using string constructor { 'H', 'e', 'l', 'l','o' };
+        string greetings = new string(letters);
+        Console.WriteLine( greetings );
+
+        //methods returning string { "Hello", "From", "Tutorials", "Point" };
+        string message = String.Join(" ", sarray);
+        Console.WriteLine( message );
+        //formatting method to convert a value
+        message = String.Format( "Formatting {0} and {1}", fname, lname );
+        Console.WriteLine( message );
+    }
+    /** Aliases in C#
+    object:  System.Object
+    string:  System.String
+    bool:    System.Boolean
+    byte:    System.Byte
+    sbyte:   System.SByte
+    short:   System.Int16
+    ushort:  System.UInt16
+    int:     System.Int32
+    uint:    System.UInt32
+    long:    System.Int64
+    ulong:   System.UInt64
+    float:   System.Single
+    double:  System.Double
+    decimal: System.Decimal
+    char:    System.Char
+    */
+}
---
+++ UnderstandingUnityEngine/Basic Reference Files/Basic05 interface.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    public interface ITransactions
+    {
+        // interface members
+        void showTransaction();
+        double getAmount();
+    }
+
+    public class Transaction : ITransactions
+    {
+        private string tCode;
+        private string date;
+        private double amount;
+
+        public Transaction()
+        {
+            tCode = " ";
+            date = " ";
+            amount = 0.0;
+        }
+        public Transaction( string c, string d, double a )
+        {
+            tCode = c;
+            date = d;
+            amount = a;
+        }
+        public double getAmount()
+        {
+            return amount;
+        }
+        public void showTransaction()
+        {
+            Console.WriteLine( "Transaction: {0}", tCode );
+            Console.WriteLine( "Date: {0}", date );
+            Console.WriteLine( "Amount: {0}", getAmount() );
+        }
+    }
+    class Tester
+    {
+        static void Main( string[] args )
+        {
+            Transaction t1 = new Transaction("001", "8/10/2012", 78900.00);
+            Transaction t2 = new Transaction("002", "9/10/2012", 451900.00);
+
+            t1.showTransaction();
+            t2.showTransaction();
+            Console.ReadKey();
+        }
+    }
+}
