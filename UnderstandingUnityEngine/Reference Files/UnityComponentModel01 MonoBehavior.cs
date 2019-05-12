aa40d5199551bf51165a70b0e6dbf5a41fd4f4ff
jintaeks <jintaeks@gmail.com>
2019-05-13 00:21:05 +0900

    Added UnityComponentModel Steps.

---
+++ UnderstandingUnityEngine/Reference Files/UnityComponentModel01 MonoBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace UnderstandingUnityEngine
+{
+    public abstract class Component
+    {
+        public GameObject gameObject { get; set; }
+        public void SendMessage( string methodName ) { }
+    }
+
+    public class Behavior : Component
+    {
+    }
+
+    public class MonoBehavior : Behavior
+    {
+        public void Invoke( string methodName ) { }
+    }
+
+    public class BoxCollider : MonoBehavior
+    {
+        public void GetCollider() { }
+    }
+
+    public class MeshRenderer : MonoBehavior
+    {
+        public void GetMaterial()
+        {
+            Console.WriteLine( "GetMaterial" );
+        }
+    }
+
+    public class GameObject
+    {
+    }
+
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            GameObject e = new GameObject();
+        }
+    }
+}
---
+++ UnderstandingUnityEngine/Reference Files/UnityComponentModel02 AddComponent.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace UnderstandingUnityEngine
+{
+    public abstract class Component
+    {
+        public GameObject gameObject { get; set; }
+        public void SendMessage( string methodName ) { }
+    }
+
+    public class Behavior : Component
+    {
+    }
+
+    public class MonoBehavior : Behavior
+    {
+        public void Invoke( string methodName ) { }
+    }
+
+    public class BoxCollider : MonoBehavior
+    {
+        public void GetCollider() { }
+    }
+
+    public class MeshRenderer : MonoBehavior
+    {
+        public void GetMaterial()
+        {
+            Console.WriteLine( "GetMaterial" );
+        }
+    }
+
+    public class GameObject
+    {
+        private List<Component> m_components;
+
+        public GameObject()
+        {
+            m_components = new List<Component>();
+
+            AddComponent<BoxCollider>();
+            AddComponent<MeshRenderer>();
+        }
+
+        public T AddComponent<T>() where T : Component, new()
+        {
+            T component = new T();
+            component.gameObject = this;
+            m_components.Add( component );
+            return component;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            GameObject e = new GameObject();
+        }
+    }
+}
