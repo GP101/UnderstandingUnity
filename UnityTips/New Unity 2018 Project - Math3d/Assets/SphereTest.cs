19de05eab5544d00b0478fb21024f60579965d06
jintaeks <jintaeks@gmail.com>
2019-05-30 16:31:17 +0900

    Added Uniyt Math3d Project.

---
+++ UnityTips/New Unity 2018 Project - Math3d/Assets/SphereTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereTest : MonoBehaviour
+{
+    public Transform _plane;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 hitPointOnPlane = Math3d.ProjectPointOnPlane(_plane.up, _plane.position
+            , transform.position);
+        Debug.DrawLine(transform.position, hitPointOnPlane, Color.red );
+    }
+}
