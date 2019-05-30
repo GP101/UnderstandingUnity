33e3a63a132741d18df5b0548916676f60f8eebb
jintaeks <jintaeks@gmail.com>
2019-05-30 15:42:03 +0900

    Added Unity jittering test project.

---
+++ UnityTips/New Unity 2018 Project - jittering/Assets/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform _target;
+    public Vector3 _offset = Vector3.zero;
+    private Vector3 _lerpedTarget;
+    private float _lerpFactor = 2f;
+    private bool _isFixedUpdate = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lerpedTarget = _target.position;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    void FixedUpdate()
+    {
+        _lerpedTarget = Vector3.Lerp(_lerpedTarget, _target.position, Time.deltaTime * _lerpFactor);
+        gameObject.transform.LookAt(_lerpedTarget);
+        gameObject.transform.position = _lerpedTarget + _offset;
+        _isFixedUpdate = true;
+    }
+    private void LateUpdate()
+    {
+        if (_isFixedUpdate == false)
+        {
+            Vector3 newPos = Vector3.Lerp(_lerpedTarget, gameObject.transform.position, 0.0f);
+            gameObject.transform.position = newPos + _offset;
+        }
+        _isFixedUpdate = false;
+    }
+}
---
+++ UnityTips/New Unity 2018 Project - jittering/Assets/ChangeVelocity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeVelocity : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float _speedFactor = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        _rigidbody.velocity = new Vector3(0, 0, h * _speedFactor);
+    }
+}
