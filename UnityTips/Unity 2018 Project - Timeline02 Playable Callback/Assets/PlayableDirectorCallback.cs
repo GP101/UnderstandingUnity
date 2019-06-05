58f0942b9570c1ef9cbc4dc173a0b4efebb8dfd1
jintaeks <jintaeks@gmail.com>
2019-06-05 20:55:06 +0900

    Unity Project Timeline Playable Callback Demo.

---
+++ UnityTips/Unity 2018 Project - Timeline02 Playable Callback/Assets/PlayableDirectorCallback.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlayableDirectorCallback : MonoBehaviour
+{
+    public Vector3 _position;
+    public PlayableDirector _director;
+
+    void OnEnable()
+    {
+        _director.stopped += OnPlayableDirectorStopped;
+    }
+
+    void OnPlayableDirectorStopped(PlayableDirector aDirector)
+    {
+        if (_director == aDirector)
+        {
+            Debug.Log("PlayableDirector named " + aDirector.name + " is now stopped.");
+            gameObject.AddComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>();
+            //gameObject.AddComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>();
+        }
+    }
+
+    void OnDisable()
+    {
+        _director.stopped -= OnPlayableDirectorStopped;
+    }
+
+    private void OnGUI()
+    {
+        string text = string.Format("Pos=({0},{1},{2})"
+            , transform.position.x, transform.position.y, transform.position.z);
+        int y = 0;
+        GUI.Label(new Rect(0, y, 200, 20), text);
+        y += 20;
+        //if (GUI.Button(new Rect(0, y, 200, 20), "Play"))
+        //{
+        //    transform.rotation = Quaternion.identity;
+        //    _director.Play();
+        //}
+    }
+}
---
+++ UnityTips/Unity 2018 Project - Timeline02 Playable Callback/Assets/StartTimeline.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class StartTimeline : MonoBehaviour
+{
+    public PlayableDirector _director;
+    public Transform _player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter named " + other.gameObject.name);
+        if (_director != null && _director.state != PlayState.Playing)
+        {
+            Destroy(_player.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>());
+            Destroy(_player.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>());
+            _player.transform.rotation = Quaternion.identity;
+
+            _director.Play();
+        }
+    }
+}
