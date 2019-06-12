fbb704c116021c8908cd07540a2ff14881bca701
jintaeks <jintaeks@gmail.com>
2019-06-12 16:43:02 +0900

    Added two unity projects. 1 FindGameObjectWithTag Problem 2 NavMesh01 Basic

---
+++ UnityTips/Unity 2018 Project - FindGameObjectWithTag Problem/Assets/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    GameObject _player;
+    string _componentsName="";
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        Component[] components = gameObject.GetComponents(typeof(Component));
+        foreach (Component component in components)
+        {
+            _componentsName += component.ToString();
+            _componentsName += "\n";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 400, 20), _player.name);
+        GUI.Label(new Rect(0, 20, 400, 200), _componentsName);
+    }
+}
---
+++ UnityTips/Unity 2018 Project - NavMesh01 Basic/Assets/CapsuleController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CapsuleController : MonoBehaviour
+{
+    Camera _cam;
+    NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cam = GameObject.FindGameObjectWithTag("MainCamera").gameObject.GetComponent<Camera>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool isHit = Physics.Raycast(ray, out hit);
+            if (isHit)
+            {
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
