a4b872bceb08ca414a3e637eaa29d1546d0cf921
jintaeks <jintaeks@gmail.com>
2019-06-13 22:48:42 +0900

    Added Unity Anim Events Project.

---
+++ UnityAnimations/New Unity 2018 Project - Anim Events/Assets/ExampleClass.cs
@@ -0,0 +1,11 @@
+﻿// This C# function can be called by an Animation Event
+using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public void PrintEvent( string s )
+    {
+        Debug.Log( "PrintEvent: " + s + " called at: " + Time.time );
+    }
+}
