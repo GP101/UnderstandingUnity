2b742262815df2f7b4bfb83e8fb28e35d9ce4bbe
jintaeks <jintaeks@gmail.com>
2019-10-24 00:10:01 +0900

    Sources at CSharpApplication folder is modified.

--- CSharpApplication/CSharpApplication/StepByStepFiles/Program03 NullCoalesce.cs
+++ CSharpApplication/CSharpApplication/StepByStepFiles/Program03 NullCoalesce.cs
@@ -1,5 +1,12 @@
+using System;
+
 class NullCoalesce
 {
+    class KPerson
+    {
+        public string FirstName;
+    }
+
     static int? GetNullableInt()
     {
         return null;
@@ -12,8 +19,24 @@ class NullCoalesce
 
     static void Main()
     {
+        Nullable<int> i = null;
+        if( i.HasValue == true )
+            Console.WriteLine( i.Value );
+        i = 9;
+        if( i.HasValue == true )
+            Console.WriteLine( "i=" + i.Value.ToString() );
+
+        // nullable int type
+        int? j = null;
+        if( j != null )
+            Console.WriteLine( j );
+        j = 99;
+        if( j != null )
+            Console.WriteLine( "j=" + j.ToString() );
+
         int? x = null;
 
+        // null coalesce operator
         // Set y to the value of x if x is NOT null; otherwise,
         // if x == null, set y to -1.
         int y = x ?? -1;
@@ -21,11 +44,16 @@ class NullCoalesce
         // Assign i to return value of the method if the method's result
         // is NOT null; otherwise, if the result is null, set i to the
         // default value of int.
-        int i = GetNullableInt() ?? default(int);
+        int k = GetNullableInt() ?? default( int );
 
         string s = GetStringValue();
         // Display the value of s if s is NOT null; otherwise, 
         // display the string "Unspecified".
-        Console.WriteLine(s ?? "Unspecified");
+        Console.WriteLine( s ?? "Unspecified" );
+
+        // conditional null operator
+        KPerson person = null;
+        var first = person?.FirstName ?? "(unknown)";
+        Console.WriteLine( first );
     }
 }
